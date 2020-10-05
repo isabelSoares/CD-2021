8657c33c2254af8e8c41c871d388e769cd0defb4
Rodrigo Sousa <rod_sousa@hotmail.com>
2020-10-05 10:38:04 +0100

    Granularity

---
+++ Labs/Lab01/QOT_granularity.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import ds_functions as ds
+
+graphsDir = './Results/'
+
+data = pd.read_csv('../../Dataset/qsar_oral_toxicity.csv', sep=';', header=None)
+values = {'nr records': data.shape[0], 'nr variables': data.shape[1]}
+print(values)
+
+print("Computing QOT Granularity - 10bins ...")
+variables = data.select_dtypes(include='number').columns
+rows, cols = ds.choose_grid(len(variables), 25)
+fig, axs = plt.subplots(rows, cols, figsize=(cols*ds.HEIGHT, rows*ds.HEIGHT))
+i, j = 0, 0
+for n in range(len(variables)):
+    axs[i, j].set_title('Histogram for %s'%variables[n])
+    axs[i, j].set_xlabel(variables[n])
+    axs[i, j].set_ylabel('nr records')
+    axs[i, j].hist(data[variables[n]].values, bins=10)
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+plt.savefig(graphsDir + 'QOT Granularity - 10bins.png')
+
+print("Finished Calculations for QOT Granularity.")
