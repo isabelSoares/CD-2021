ad879f847a0c7213f8bcd39d86f91610261dfc3a
Rodrigo Sousa <rod_sousa@hotmail.com>
2020-10-05 15:31:41 +0100

    Updated Results

--- Labs/Lab01/HFCR_granularity.py
+++ Labs/Lab01/HFCR_granularity.py
@@ -14,11 +14,12 @@ rows, cols = ds.choose_grid(len(variables))
 fig, axs = plt.subplots(rows, cols, figsize=(cols*ds.HEIGHT, rows*ds.HEIGHT))
 i, j = 0, 0
 for n in range(len(variables)):
-    axs[i, j].set_title('Histogram for %s'%variables[n])
+    axs[i, j].set_title('Histogram for %s'%variables[n], color='chocolate')
     axs[i, j].set_xlabel(variables[n])
     axs[i, j].set_ylabel('nr records')
-    axs[i, j].hist(data[variables[n]].values, bins=100)
+    axs[i, j].hist(data[variables[n]].values, bins=100, color='peachpuff')
     i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.suptitle("HFCR Granularity - 100 Bins per Variable", color='chocolate')
 plt.savefig(graphsDir + 'HFCR Granularity - 100bins.png')
 
 print("Computing HFCR Granularity - 10.100.1000bins ...")
@@ -29,10 +30,11 @@ cols = len(bins)
 fig, axs = plt.subplots(rows, cols, figsize=(cols*ds.HEIGHT, rows*ds.HEIGHT))
 for i in range(rows):
     for j in range(cols):
-        axs[i, j].set_title('Histogram for %s %d bins'%(columns[i], bins[j]))
+        axs[i, j].set_title('Histogram for %s %d bins'%(columns[i], bins[j]), color='chocolate')
         axs[i, j].set_xlabel(columns[i])
         axs[i, j].set_ylabel('Nr records')
-        axs[i, j].hist(data[columns[i]].values, bins=bins[j])
+        axs[i, j].hist(data[columns[i]].values, bins=bins[j], color='peachpuff')
+fig.suptitle("HFCR Granularity - 10, 100, 1000 Bins per Variable", color='chocolate')
 plt.savefig(graphsDir + 'HFCR Granularity - 10.100.1000bins.png')
 
 print("Computing HFCR Granularity - BestFit bins ...")
@@ -42,11 +44,12 @@ rows, cols = ds.choose_grid(len(variables))
 fig, axs = plt.subplots(rows, cols, figsize=(cols*ds.HEIGHT, rows*ds.HEIGHT))
 i, j = 0, 0
 for n in range(len(variables)):
-    axs[i, j].set_title('Histogram for %s - %d bins'%(variables[n], bins[n]))
+    axs[i, j].set_title('Histogram for %s - %d bins'%(variables[n], bins[n]), color='chocolate')
     axs[i, j].set_xlabel(variables[n])
     axs[i, j].set_ylabel('nr records')
-    axs[i, j].hist(data[variables[n]].values, bins=bins[n])
+    axs[i, j].hist(data[variables[n]].values, bins=bins[n], color='peachpuff')
     i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.suptitle("HFCR Granularity - Best Fit Bins per Variable", color='chocolate')
 plt.savefig(graphsDir + 'HFCR Granularity - BestFit bins.png')
 
 print("Finished Calculations for HFCR Granularity.")
--- Labs/Lab01/QOT_granularity.py
+++ Labs/Lab01/QOT_granularity.py
@@ -14,11 +14,12 @@ rows, cols = ds.choose_grid(len(variables), 25)
 fig, axs = plt.subplots(rows, cols, figsize=(cols*ds.HEIGHT, rows*ds.HEIGHT))
 i, j = 0, 0
 for n in range(len(variables)):
-    axs[i, j].set_title('Histogram for %s'%variables[n])
+    axs[i, j].set_title('Histogram for %s'%variables[n], color='chocolate')
     axs[i, j].set_xlabel(variables[n])
     axs[i, j].set_ylabel('nr records')
-    axs[i, j].hist(data[variables[n]].values, bins=10)
+    axs[i, j].hist(data[variables[n]].values, bins=10, color='peachpuff')
     i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.suptitle("QOT Granularity - 10 Bins per Variable", color='chocolate')
 plt.savefig(graphsDir + 'QOT Granularity - 10bins.png')
 
 print("Finished Calculations for QOT Granularity.")
