6232a10435e81b0528b7c6b726bbb41285133239
isabelSoares <isabel.srsoares@gmail.com>
2020-10-08 10:45:27 +0100

    Added correlation analytics

---
+++ Labs/Lab02/HFRC_correlation.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+
+graphsDir = './Results/'
+
+fig = plt.figure(figsize=[12, 12])
+corr_mtx = data.corr()
+sns.heatmap(corr_mtx, xticklabels=corr_mtx.columns, yticklabels=corr_mtx.columns, annot=True, cmap='Blues')
+plt.title('Correlation analysis')
+plt.show(graphsDir + 'HFCR - Correlation analysis')
