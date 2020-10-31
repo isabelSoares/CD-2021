2986105943f830b21e1e6c05a3584c469b1116de
João Dias <joao.dias12321@gmail.com>
2020-10-31 20:32:50 +0000

    random forests

---
+++ Labs/Lab05/QOT_random_forests.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import sklearn.metrics as metrics
+from sklearn.ensemble import RandomForestClassifier
+import ds_functions as ds
+import os
+
+graphsDir = './Results/'
+if not os.path.exists(graphsDir):
+    os.makedirs(graphsDir)
+
+
+print('------------------------------')
+print('-                            -')
+print('-     QOT Random Forests     -')
+print('-                            -')
+print('------------------------------')
+
+
+
+data: pd.DataFrame = pd.read_csv('../../Dataset/qsar_oral_toxicity.csv', sep=';', header=None)
+
+print('QOT Random Forests - Parameters Combinations')
+y: np.ndarray = data.pop(1024).values
+X: np.ndarray = data.values
+labels = pd.unique(y)
+
+trnX, tstX, trnY, tstY = train_test_split(X, y, train_size=0.7, stratify=y)
+
+n_estimators = [5, 10, 25, 50, 75, 100, 150, 200, 250, 300]
+max_depths = [5, 10, 25]
+max_features = [.1, .3, .5, .7, .9, 1]
+best = ('', 0, 0)
+last_best = 0
+best_tree = None
+
+cols = len(max_depths)
+plt.figure()
+fig, axs = plt.subplots(1, cols, figsize=(cols*ds.HEIGHT*2.5, ds.HEIGHT), squeeze=False)
+for k in range(len(max_depths)):
+    d = max_depths[k]
+    values = {}
+    for f in max_features:
+        yvalues = []
+        for n in n_estimators:
+            rf = RandomForestClassifier(n_estimators=n, max_depth=d, max_features=f)
+            rf.fit(trnX, trnY)
+            prdY = rf.predict(tstX)
+            yvalues.append(metrics.accuracy_score(tstY, prdY))
+            if yvalues[-1] > last_best:
+                best = (d, f, n)
+                last_best = yvalues[-1]
+                best_tree = rf
+
+        values[f] = yvalues
+    ds.multiple_line_chart(n_estimators, values, ax=axs[0, k], title='Random Forests with max_depth=%d'%d,
+                           xlabel='nr estimators', ylabel='accuracy', percentage=True)
+
+plt.suptitle('QOT Random Forests - Parameters Combinations')
+plt.savefig(graphsDir + 'QOT Random Forests - Parameters Combinations')
+print('Best results with depth=%d, %1.2f features and %d estimators, with accuracy=%1.2f'%(best[0], best[1], best[2], last_best))
+print()
+
+print('QOT Random Forests - Performance & Confusion Matrix')
+prd_trn = best_tree.predict(trnX)
+prd_tst = best_tree.predict(tstX)
+ds.plot_evaluation_results(pd.unique(y), trnY, prd_trn, tstY, prd_tst)
+plt.suptitle('QOT Random Forests - Performance & Confusion Matrix')
+plt.savefig(graphsDir + 'QOT Random Forests - Performance & Confusion Matrix')
+print()
